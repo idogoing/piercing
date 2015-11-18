7ee4d1c990abeb7ef4c35fa6dcbd67a46725df31
Leon <leon@LeondeMacBook-Pro.local>
2015-11-19 00:18:08 +0800

    update

--- src/com/bajie/main/Piercing.java
+++ src/com/bajie/main/Piercing.java
@@ -3,8 +3,7 @@ package com.bajie.main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.bajie.config.ConfigUtil;
-import com.vguang.VguangApi;
+import com.bajie.swing.Swing;
 
 public class Piercing {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Piercing.class);
@@ -13,13 +12,15 @@ public class Piercing {
 	public static void main(String[] args) {
 		LOGGER.info("开始启动");
 		piercing = new Piercing();
+		Swing.vguangSample = new Swing();
+		Swing.vguangSample.frame.setVisible(true);
 		//设置参数设置
 		LOGGER.info("开始设置打印机参数");
-		ConfigUtil.applySetting();
-		VguangApi.beep(2);
+		//ConfigUtil.applySetting();
+		//VguangApi.beep(2);
 		LOGGER.info("打印机参数设置完毕,响铃2秒");
-		while(true){
-			
-		}
+		
 	}
+	
+	
 }
